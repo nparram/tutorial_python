4e5b2df8ada9d812e2d7572fcebd162dd06a6d16
nilson <n.parram@uniandes.edu.co>
2021-02-24 08:26:35 -0500

    add classes gui and logic

---
+++ src/myapp/__main__.py
@@ -0,0 +1,17 @@
+import sys
+from gui.visor_materias import Aplicacion_Gui
+from PyQt5.QtWidgets import QApplication
+
+from logica.organizador_materias import Organizador_Materias
+
+
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.logica = Organizador_Materias()
+        self.gui = Aplicacion_Gui(self.logica)
+
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    sys.exit(app.exec_())
